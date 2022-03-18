e6e5bbd341c330a43c47875cd94ea18122291bca
Sajin Shrestha <sajinshrestha74@gmail.com>
2022-03-18 11:30:35 -0500

    Staring payment process

--- frontend/src/userAccountComponents/EditSections/index.js
+++ frontend/src/userAccountComponents/EditSections/index.js
@@ -1,13 +1,21 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 
 const EditSections = () => {
   return (
-
     <div className="container rounded bg-white mt-5 mb-5">
       <div className="row">
         <div className="col-md-3 border-right">
-          <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span className="font-weight-bold">Edogaru</span><span className="text-black-50">edogaru@mail.com.my</span><span> </span></div>
+          <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+            <img
+              className="rounded-circle mt-5"
+              width="150px"
+              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+            />
+            <span className="font-weight-bold">Edogaru</span>
+            <span className="text-black-50">edogaru@mail.com.my</span>
+            <span> </span>
+          </div>
         </div>
         <div className="col-md-5 border-right">
           <div className="p-3 py-5">
@@ -15,31 +23,155 @@ const EditSections = () => {
               <h4 className="text-right">Profile Settings</h4>
             </div>
             <div className="row mt-2">
-              <div className="col-md-6"><label className="labels">Name</label><input type="text" className="form-control" placeholder="first name" defaultValue /></div>
-              <div className="col-md-6"><label className="labels">Surname</label><input type="text" className="form-control" defaultValue placeholder="surname" /></div>
+              <div className="col-md-6">
+                <label className="labels">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="first name"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-6">
+                <label className="labels">Surname</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  defaultValue
+                  placeholder="surname"
+                />
+              </div>
             </div>
             <div className="row mt-3">
-              <div className="col-md-12"><label className="labels">Mobile Number</label><input type="text" className="form-control" placeholder="enter phone number" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Address Line 1</label><input type="text" className="form-control" placeholder="enter address line 1" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Address Line 2</label><input type="text" className="form-control" placeholder="enter address line 2" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Postcode</label><input type="text" className="form-control" placeholder="enter address line 2" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">State</label><input type="text" className="form-control" placeholder="enter address line 2" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Area</label><input type="text" className="form-control" placeholder="enter address line 2" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Email ID</label><input type="text" className="form-control" placeholder="enter email id" defaultValue /></div>
-              <div className="col-md-12"><label className="labels">Education</label><input type="text" className="form-control" placeholder="education" defaultValue /></div>
+              <div className="col-md-12">
+                <label className="labels">Mobile Number</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter phone number"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Address Line 1</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter address line 1"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Address Line 2</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter address line 2"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Postcode</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter address line 2"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">State</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter address line 2"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Area</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter address line 2"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Email ID</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="enter email id"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-12">
+                <label className="labels">Education</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="education"
+                  defaultValue
+                />
+              </div>
             </div>
             <div className="row mt-3">
-              <div className="col-md-6"><label className="labels">Country</label><input type="text" className="form-control" placeholder="country" defaultValue /></div>
-              <div className="col-md-6"><label className="labels">State/Region</label><input type="text" className="form-control" defaultValue placeholder="state" /></div>
+              <div className="col-md-6">
+                <label className="labels">Country</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="country"
+                  defaultValue
+                />
+              </div>
+              <div className="col-md-6">
+                <label className="labels">State/Region</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  defaultValue
+                  placeholder="state"
+                />
+              </div>
             </div>
-            <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="button">Save Profile</button></div>
+            <div className="mt-5 text-center">
+              <button className="btn btn-primary profile-button" type="button">
+                Save Profile
+              </button>
+            </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="p-3 py-5">
-            <div className="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span className="border px-3 p-1 add-experience"><i className="fa fa-plus" />&nbsp;Experience</span></div><br />
-            <div className="col-md-12"><label className="labels">Experience in Designing</label><input type="text" className="form-control" placeholder="experience" defaultValue /></div> <br />
-            <div className="col-md-12"><label className="labels">Additional Details</label><input type="text" className="form-control" placeholder="additional details" defaultValue /></div>
+            <div className="d-flex justify-content-between align-items-center experience">
+              <span>Edit Experience</span>
+              <span className="border px-3 p-1 add-experience">
+                <i className="fa fa-plus" />
+                &nbsp;Experience
+              </span>
+            </div>
+            <br />
+            <div className="col-md-12">
+              <label className="labels">Experience in Designing</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="experience"
+                defaultValue
+              />
+            </div>{" "}
+            <br />
+            <div className="col-md-12">
+              <label className="labels">Additional Details</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="additional details"
+                defaultValue
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -48,4 +180,3 @@ const EditSections = () => {
 };
 
 export default EditSections;
-
